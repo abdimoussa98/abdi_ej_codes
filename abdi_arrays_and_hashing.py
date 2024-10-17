f080937cd9e45bc138a24ecd01bedcac5fe9077b
Abdulrahman Moussa <abdimoussa98@gmail.com>
2024-10-16 20:04:04 -0500

    add first solution
    Co-authored-by: Eun Yi <eyi1@users.noreply.github.com>

--- abdi_arrays_and_hashing.py
+++ abdi_arrays_and_hashing.py
@@ -1,3 +1,16 @@
 # https://leetcode.com/problems/contains-duplicate/description/
 
 
+def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        check = set()
+        for num in nums:
+            if num in check:
+                return True
+            else: 
+                check.add(num)
+    
+        return False
--- ej_arrays_and_hashing.py
+++ ej_arrays_and_hashing.py
@@ -1,3 +1,16 @@
 # https://leetcode.com/problems/contains-duplicate/description/
 
 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+      
+         for currEl in nums: 
+            nextEl = currEl + 1
+
+            for nextEl in nums: 
+                # if nums.index(nextEl) == len(nums)-1: break
+                if currEl == nextEl:
+                    return True
+                nextEl = nextEl + 1
+            
+            currEl = currEl + 1

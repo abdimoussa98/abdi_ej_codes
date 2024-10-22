220564632fdc50d28ad24f13f41b2f67b6dfbaa5
Abdulrahman Moussa <abdimoussa98@gmail.com>
2024-10-21 19:06:18 -0500

    add validAnagram solutions

--- abdi_arrays_and_hashing.py
+++ abdi_arrays_and_hashing.py
@@ -1,6 +1,4 @@
 # https://leetcode.com/problems/contains-duplicate/description/
-
-
 def containsDuplicate(self, nums):
         """
         :type nums: List[int]
@@ -14,3 +12,26 @@ def containsDuplicate(self, nums):
                 check.add(num)
     
         return False
+
+# https://leetcode.com/problems/valid-anagram/description/
+def isAnagram(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+
+    freq_s = [0] * 26
+    freq_t = [0] * 26
+
+    for c in s: # o(n)
+        freq_s[ord(c) - ord('a')] += 1
+    for c in t: # o(n)
+        freq_t[ord(c) - ord('a')] += 1
+
+    print(freq_s)
+    print(freq_t)
+
+    if freq_s == freq_t:
+        return True
+    return False
--- ej_arrays_and_hashing.py
+++ ej_arrays_and_hashing.py
@@ -1,16 +1,27 @@
 # https://leetcode.com/problems/contains-duplicate/description/
+def hasDuplicate(self, nums: List[int]) -> bool:
 
+    for currEl in nums: 
+        nextEl = currEl + 1
 
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-      
-         for currEl in nums: 
-            nextEl = currEl + 1
+    for nextEl in nums: 
+        # if nums.index(nextEl) == len(nums)-1: break
+        if currEl == nextEl:
+            return True
+        nextEl = nextEl + 1
+    
+    currEl = currEl + 1
 
-            for nextEl in nums: 
-                # if nums.index(nextEl) == len(nums)-1: break
-                if currEl == nextEl:
-                    return True
-                nextEl = nextEl + 1
-            
-            currEl = currEl + 1
+# https://leetcode.com/problems/valid-anagram/description/
+def isAnagram(self, s: str, t: str) -> bool:
+    sortedS = sorted(s) #n log n
+    sortedT = sorted(t) # n log n
+    
+    return sortedS == sortedT #o(n)
+    # if len(sortedS) == len(sortedT)
+    #     for i in range(len(sortedS)):
+    #         if sortedS[i] != sortedT[i]:
+    #             return False
+    #     return True
+    
+    # return False
